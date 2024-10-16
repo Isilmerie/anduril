562d0b6efd0a7686b6b645bf957e0a244ab05310
Isilmerie <iankim223@gmail.com>
2024-10-15 21:18:07 -0400

    Add "has_secondary_args" to d4k-3ch channels

--- hw/hank/emisar-d4k-3ch/hwdef.c
+++ hw/hank/emisar-d4k-3ch/hwdef.c
@@ -34,54 +34,64 @@ Channel channels[] = {
         .set_level    = set_level_main2,
         .gradual_tick = gradual_tick_main2,
         .has_args     = 0,
+        .has_secondary_args = 0,
         .uses_aux     = 0
     },
     { // 3rd LED only
         .set_level    = set_level_led3,
         .gradual_tick = gradual_tick_led3,
         .has_args     = 0,
+        .has_secondary_args = 0,
         .uses_aux     = 0
     },
     { // 4th LED only
         .set_level    = set_level_led4,
         .gradual_tick = gradual_tick_led4,
         .has_args     = 0,
+        .has_secondary_args = 0,
         .uses_aux     = 0
     },
     { // all channels, tied together (equal amounts, max power)
         .set_level    = set_level_all,
         .gradual_tick = gradual_tick_all,
         .has_args     = 0,
+        .has_secondary_args = 0,
         .uses_aux     = 0
     },
     { // 3rd + 4th LEDs (8/16/16) or 3rd + main LEDs (16/16/8), manual blend (max "100%" power)
         .set_level    = set_level_led34a_blend,
         .gradual_tick = gradual_tick_led34a_blend,
         .has_args     = 1,
+        .has_secondary_args = 0,
         .uses_aux     = 0
     },
     { // main + 3rd LEDs (8/16/16) or 4th + 3rd LEDs (16/16/8), manual blend (max "100%" power)
         .set_level    = set_level_led34b_blend,
         .gradual_tick = gradual_tick_led34b_blend,
         .has_args     = 1,
+        .has_secondary_args = 0,
         .uses_aux     = 0
     },
     { // main + 4th LEDs (8/16/16) or 4th + main LEDs (16/16/8), manual blend (max "100%" power)
         .set_level    = set_level_led14_blend,
         .gradual_tick = gradual_tick_led14_blend,
-        .has_args     = 1
+        .has_args     = 1,
+        .has_secondary_args = 0,
+        .uses_aux     = 0
     },
 
     { // 3ch blend (HSV style)
         .set_level    = set_level_hsv,
         .gradual_tick = gradual_tick_hsv,
         .has_args     = 1,
+        .has_secondary_args = 1,
         .uses_aux     = 0
     },
     { // 3ch auto blend (red-warm-cool style, led4-led3-main2)
         .set_level    = set_level_auto3,
         .gradual_tick = gradual_tick_auto3,
         .has_args     = 0,
+        .has_secondary_args = 0,
         .uses_aux     = 0
     },
     RGB_AUX_CHANNELS
